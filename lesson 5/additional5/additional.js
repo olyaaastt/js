ff85c53b6f2097503cdc21d7b677162eba953c16
Olya <91497096+olyaaastt@users.noreply.github.com>
2021-11-23 14:15:01 +0200

    add#5

--- lesson 5/additional5/additional.js
+++ lesson 5/additional5/additional.js
@@ -75,24 +75,26 @@
 
 
 
-// let foo =(arr)=> {
-//     let c = 0;
-//     for (let i = 0; i < arr.length; i++){
-//         if(arr[i] !== 0){
-//             arr[c++] = arr[i];
+//let foo =(arr)=> {
+//     if(arr.length>2 && arr.length<100) {
+//         let c = 0;
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] !== 0) {
+//                 arr[c++] = arr[i];
+//
+//             }
 //
 //         }
 //
-//     }
+//         for (let i = c; i < arr.length; i++) {
+//             arr[i] = 0;
 //
-//     for (let i = c; i < arr.length; i++){
-//         arr[i] = 0;
-//
+//         }
+//         console.log(arr);
+//         return arr;
 //     }
-//     console.log(arr);
-//     return arr;
+//     else return 0;
 // }
-//
 // foo([1,0,6,0,3]);
 // foo([0,1,2,3,4]);
 // foo([0,0,1,0]);
